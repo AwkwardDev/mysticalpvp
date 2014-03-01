4dc3666e5342c4de9238031590f5960b14bf65c3
Hezekiahqt <hezekiah@europe.com>
2014-03-01 03:44:02 +0200

    Added.

--- src/server/scripts/Custom/PlayerScript/OnDuel_PlayerScript.cpp
+++ src/server/scripts/Custom/PlayerScript/OnDuel_PlayerScript.cpp
@@ -12,10 +12,20 @@ public:
 
     void OnDuelStart(Player * winner, Player * loser)
     {
-        if (winner->getPowerType() == POWER_ENERGY)
-            winner->SetPower(POWER_ENERGY, winner->GetMaxPower(POWER_ENERGY));
-        if (winner->getPowerType() == POWER_RAGE)
-            winner->SetPower(POWER_RAGE, 0);
+        winner->AttackStop();
+        winner->CastStop();
+        winner->RemoveAurasByType(SPELL_AURA_PERIODIC_DAMAGE);
+        winner->RemoveArenaSpellCooldowns();
+        winner->SetHealth(winner->GetMaxHealth());
+        if (winner->getPowerType() == POWER_MANA)
+            winner->SetPower(POWER_MANA, winner->GetMaxPower(POWER_MANA));
+        loser->AttackStop();
+        loser->CastStop();
+        loser->RemoveAurasByType(SPELL_AURA_PERIODIC_DAMAGE);
+        loser->RemoveArenaSpellCooldowns();
+        loser->SetHealth(loser->GetMaxHealth());
+        if (loser->getPowerType() == POWER_MANA)
+            loser->SetPower(POWER_MANA, loser->GetMaxPower(POWER_MANA));
     }
 
     void OnDuelEnd(Player * winner, Player * loser, DuelCompleteType /*type*/)
